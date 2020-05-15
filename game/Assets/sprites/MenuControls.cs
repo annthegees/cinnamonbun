244ca054a436bfb4a5854372f6d9e5ce215ba15d
Semertak <govorov-roman@mail.ru>
2020-05-15 16:44:29 +0400

    Склепил сцены. Немного навел порядок.

---
+++ game/Assets/sprites/MenuControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+        public class MenuControls : MonoBehaviour
+
+{
+    
+ 
+        public void PlayPressed()
+
+    {
+
+        SceneManager.LoadScene("Scenes/game");
+
+    }
+
+    public void ExitPressed()
+
+    {
+
+        Debug.Log("Exit pressed!");
+        Application.Quit();
+
+        
+    }
+    
+        
+    
+}
